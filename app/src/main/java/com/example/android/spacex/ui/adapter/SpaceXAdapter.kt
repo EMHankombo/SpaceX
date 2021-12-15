a60addf88ca2901efadc7307feed9471c4394bb9
Enoch <user@example.com>
2021-12-15 21:39:50 +0000

    Dates displaying correctly

--- app/src/main/java/com/example/android/spacex/ui/adapter/SpaceXAdapter.kt
+++ app/src/main/java/com/example/android/spacex/ui/adapter/SpaceXAdapter.kt
@@ -9,8 +9,13 @@ import com.example.android.spacex.databinding.LaunchItemBinding
 import com.example.android.spacex.network.model.Launch
 import com.example.android.spacex.network.model.Links
 import com.example.android.spacex.utils.Utilities
+import java.time.Instant
+import kotlin.math.abs
 
-class SpaceXAdapter(private val launches: List<Launch>,private val clickListener:(Links)->Unit = {}) :
+class SpaceXAdapter(
+    private val launches: List<Launch>,
+    private val clickListener: (Links) -> Unit = {}
+) :
     RecyclerView.Adapter<SpaceXAdapter.SpaceXViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpaceXViewHolder {
@@ -26,7 +31,7 @@ class SpaceXAdapter(private val launches: List<Launch>,private val clickListener
         return launches.size
     }
 
-   inner class SpaceXViewHolder(private val itemBinding: LaunchItemBinding) :
+    inner class SpaceXViewHolder(private val itemBinding: LaunchItemBinding) :
         RecyclerView.ViewHolder(itemBinding.root) {
 
         fun binding(launch: Launch) {
@@ -34,6 +39,16 @@ class SpaceXAdapter(private val launches: List<Launch>,private val clickListener
                 tvMission.text = launch.name
                 tvDateTime.text = Utilities.getDateOnly(launch.date_utc)
                 tvRocket.text = launch.rocket
+                val duration = if (Utilities.getDaysBetweenDates(
+                        Instant.now().toString(),
+                        launch.date_utc
+                    ) < 0
+                ) "since" else "from"
+                tvDaysHeading.text = itemBinding.root.context.getString(R.string.tv_days_heading,duration)
+                tvDays.text = abs(Utilities.getDaysBetweenDates(
+                    Instant.now().toString(),
+                    launch.date_utc
+                )).toString()
                 if (launch.success) {
                     imgLaunchSuccess.setImageResource(R.drawable.ic_baseline_check_24)
                 } else {
@@ -41,6 +56,8 @@ class SpaceXAdapter(private val launches: List<Launch>,private val clickListener
                         R.drawable.ic_baseline_clear_24
                     )
                 }
+
+
                 Glide.with(itemBinding.root).load(launch.links.patch.large).into(imgPatch)
             }
             itemBinding.root.setOnClickListener {
